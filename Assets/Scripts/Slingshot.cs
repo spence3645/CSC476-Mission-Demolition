04f1b2310837aed0d100ae4e3259c173eeb3c15b
spence3645 <spence3645@gmail.com>
2020-10-09 10:07:39 -0700

    Started scripting the slingshot

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    GameObject launchPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        launchPoint = transform.Find("LaunchPoint").gameObject;
+
+        launchPoint.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        launchPoint.SetActive(false);
+    }
+}
