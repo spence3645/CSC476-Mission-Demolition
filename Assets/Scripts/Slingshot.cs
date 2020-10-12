a057bc53762039dbfbe97c5a6fcf545ce6b882f6
spence3645 <spence3645@gmail.com>
2020-10-12 11:12:02 -0700

    Updated slingshot class

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
-    GameObject launchPoint;
+    [Header("Set in Inspector")]
+    public GameObject projectilePrefab;
+    public float velocity = 8f;
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public GameObject projectile;
+    public Rigidbody projectileRigid;
+    public Vector3 launchPos;
+    public bool b_aiming;
 
     // Start is called before the first frame update
     void Start()
     {
         launchPoint = transform.Find("LaunchPoint").gameObject;
+        launchPos = launchPoint.transform.position;
 
         launchPoint.SetActive(false);
     }
@@ -17,7 +27,33 @@ public class Slingshot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (b_aiming)
+        {
+            Vector2 mousePos = Input.mousePosition;
+
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            Vector3 mouseDelta = worldPos - launchPos;
+
+            float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+
+            if(mouseDelta.magnitude > maxMagnitude)
+            {
+                mouseDelta.Normalize();
+                mouseDelta *= maxMagnitude;
+            }
+
+            Vector3 projPos = launchPos + mouseDelta;
+            projectile.transform.position = projPos;
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                b_aiming = false;
+                projectileRigid.isKinematic = false;
+                projectileRigid.velocity = -mouseDelta * velocity;
+                projectile = null;
+            }
+        }
     }
 
     void OnMouseEnter()
@@ -28,5 +64,16 @@ public class Slingshot : MonoBehaviour
     void OnMouseExit()
     {
         launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        b_aiming = true;
+
+        projectile = Instantiate(projectilePrefab, launchPos, Quaternion.identity, null);
+
+        projectileRigid = projectile.GetComponent<Rigidbody>();
+
+        projectileRigid.isKinematic = true;
     }
 }
